dd1a71d950afb817e19efb02a65ec4e277b61cab
jordi.reverter.tuset <jordi.reverter.tuset@estudiantat.upc.edu>
2025-11-30 20:32:57 +0100

    fix id url intead of query

--- api/views/user_views.py
+++ api/views/user_views.py
@@ -4,7 +4,7 @@ Views per a la gestió d'usuaris amb endpoints REST estàndard
 import logging
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from drf_yasg.utils import swagger_auto_schema
